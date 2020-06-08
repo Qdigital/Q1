6fcbe98745632900f2768eef58f94cdcb4915a55
Qdigital <crzyazblue1@gmail.com>
2020-06-08 14:59:08 +1000

    Initialize composer-ci-install

---
+++ application/config/hooks.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|	https://codeigniter.com/user_guide/general/hooks.html
+|
+*/
+$hook['post_controller_constructor'][] = array(
+    'class'    => 'CILogger',
+    'function' => 'pre_application',
+    'filename' => 'CILogger.php',
+    'filepath' => 'libraries'
+    );
+$hook['post_controller'][] = array(
+    'class'    => 'CILogger',
+    'function' => 'post_application',
+    'filename' => 'CILogger.php',
+    'filepath' => 'libraries'
+    );
+$hook['post_system'][] = array(
+    'class'    => 'CILogger',
+    'function' => 'resolve_profiling',
+    'filename' => 'CILogger.php',
+    'filepath' => 'libraries'
+);
---
+++ application/views/changeset.php
@@ -0,0 +1,52 @@
+<?php include 'header_meta_inc_view.php';?>
+
+<?php include 'header_inc_view.php';?>
+
+    <div class="container">
+      <!-- Example row of columns -->
+
+      <div class="row">
+        <div class="col-lg-12">
+          <h2>Changeset Report Generator</h2>
+
+
+            <form action="<?php echo site_url(); ?>changeset" method="get" role="form">
+
+                <div class="form-group">
+
+                    <label>Agency</label>
+                    <select id="agency-select" name="json_old_select" class="form-control">
+                        <option value="">Select an Agency</option>
+                        <?php
+                            foreach ($orgs as $org) {
+                        ?>
+                                <option value="organization:<?php echo $org['id']; ?>"><?php echo $org['name']; ?></option>
+                        <?php
+                            }
+                        ?>
+
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="json_old">Optional: manually overide selected CKAN organizations</label>
+                    <input placeholder="organization:(eia-gov OR doe-gov OR ornl-gov OR osti-gov)" name="json_old" id="json_old" class="form-control">
+                </div>
+
+
+                <div class="form-group">
+                    <label for="datajson_new">New data.json URL</label>
+                    <input placeholder="http://energy.gov/data.json" name="datajson_new" id="datajson_new" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" value="Generate Report" class="btn btn-primary">
+                </div>
+
+            </form>
+
+
+        </div>
+    </div>
+
+<?php include 'footer.php'; ?>
